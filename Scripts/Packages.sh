3bd6e3749e10a30ac51b0949b02c38c73d7c07cf
ljxinternet <ljx5790@163.com>
2025-09-23 08:41:52 +0800

    202509230841:编译配置修改:增加CONFIG_PACKAGE_luci-app-cpufreq;

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -70,6 +70,7 @@ UPDATE_PACKAGE "qbittorrent" "sbwml/luci-app-qbittorrent" "master" "" "qt6base q
 UPDATE_PACKAGE "qmodem" "FUjr/QModem" "main"
 UPDATE_PACKAGE "viking" "VIKINGYFY/packages" "main" "" "luci-app-timewol luci-app-wolplus"
 UPDATE_PACKAGE "vnt" "lmq8267/luci-app-vnt" "main"
+#UPDATE_PACKAGE "socat" "chenmozhijin/luci-app-socat" "main"
 
 #更新软件包版本
 UPDATE_VERSION() {
